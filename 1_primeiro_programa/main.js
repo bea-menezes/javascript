8b8ee50a125f03f41ae07a38d6bef3089b515ca7
bea-menezes <beatrizprs.menezes@gmail.com>
2023-03-27 10:33:33 -0300

    [Exercício 1] - Hello World!

---
+++ 1_primeiro_programa/main.js
@@ -0,0 +1,15 @@
+
+console.log("Hello world!");
+
+var nome;
+var nome2;
+nome = "Gui";
+nome2 = "Bea";
+console.log(nome, nome2);
+
+var a, b, resultado;
+a = 1;
+b = 2;
+resultado = a+b;
+console.log(resultado);
+
